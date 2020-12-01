3087ef07d65bf7b25c2a67f3de56bde9b2076475
Raj Rajhans <raj.rajhans18@vit.edu>
2020-12-02 00:49:02 +0530

    Add Timer Componenet

---
+++ src/components/helpers/Timer.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+
+// Takes in a "duration" and "Callback Function", waits for "duration" and fires the CallbackFn
+
+class Timer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            timePassed: 0
+        }
+    }
+
+    tick() {
+        const {duration, callBackFn} = this.props;
+
+        if (this.state.timePassed === duration) {
+            callBackFn();
+        } else {
+            this.setState((prevState) => ({
+                timePassed: prevState.timePassed + 1
+            }));
+        }
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    getTimeFromSeconds = (seconds) => {
+        const minsRemaining = Math.floor(seconds / 60);
+        const secsRemaining = seconds - (minsRemaining * 60);
+
+        return `${minsRemaining} minutes and ${secsRemaining} seconds`;
+    }
+
+    render() {
+        const { duration } = this.props;
+        let timeLeftinSecs = duration - this.state.timePassed;
+
+        return (
+            <div className={"timer"}>
+                Time Remaining - {this.getTimeFromSeconds(timeLeftinSecs)}
+            </div>
+        );
+    }
+}
+
+export default Timer;
