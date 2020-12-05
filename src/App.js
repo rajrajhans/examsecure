9d50131f719214ae264bdf99f2a39d346a99e962
Raj Rajhans <raj.rajhans18@vit.edu>
2020-12-05 15:52:59 +0530

    Add prettier as dev dependency

--- src/App.js
+++ src/App.js
@@ -1,42 +1,76 @@
-import React, {Component, useState} from 'react';
+import React, { Component, useState } from "react";
 import Layout from "./components/Layout";
 import Landing from "./components/Landing";
-import './styles/main.css'
+import "./styles/main.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
-import {Router, Link, Redirect} from "@reach/router"
+import { Router, Link, Redirect } from "@reach/router";
 import PrivateRoute from "./components/helpers/PrivateRoute";
 import Exam from "./components/Exam";
 import PostSubmit from "./components/PostSubmit";
+import Loading from "./components/Loading";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {authState: undefined}
-        this.setAuthState = this.setAuthState.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { authState: undefined };
+    this.setAuthState = this.setAuthState.bind(this);
+    this.setLoading = this.setLoading.bind(this);
+    this.state = { isLoading: false };
+  }
 
-    setAuthState(s) {
-        this.setState({authState: s});
-        if(s === "signedin")
-            return <Redirect to={"/landing"}/>
-    }
+  setAuthState(s) {
+    this.setState({ authState: s });
+    if (s === "signedin") return <Redirect to={"/landing"} />;
+  }
 
-    render() {
-        const isSignedIn = this.state.authState === "signedin";
+  setLoading(val) {
+    this.state.isLoading = val;
+  }
 
-        return (
-            <Layout isSignedIn={isSignedIn}>
-                <Router>
-                    <PrivateRoute component={Landing} isSignedIn={isSignedIn} path={'/landing'}/>
-                    <Login isSignedIn={isSignedIn} authState={this.state.authState} setAuthState={this.setAuthState} path={'/'}/>
-                    <PrivateRoute component={Exam} isSignedIn={isSignedIn} path={'/exam'}/>
-                    <PrivateRoute component={PostSubmit} isSignedIn={isSignedIn} path={'/thankyou'}/>
-                </Router>
-            </Layout>
+  render() {
+    const isSignedIn = this.state.authState === "signedin";
 
-        )
-    }
+    return (
+      <Layout isSignedIn={isSignedIn}>
+        {this.state.isLoading ? (
+          <Loading />
+        ) : (
+          <Router>
+            <PrivateRoute
+              component={Landing}
+              isSignedIn={isSignedIn}
+              path={"/landing"}
+              isLoading={this.state.isLoading}
+              setIsLoading={this.setLoading}
+            />
+            <Login
+              isSignedIn={isSignedIn}
+              authState={this.state.authState}
+              setAuthState={this.setAuthState}
+              path={"/"}
+              isLoading={this.state.isLoading}
+              setIsLoading={this.setLoading}
+            />
+            <PrivateRoute
+              component={Exam}
+              isSignedIn={isSignedIn}
+              path={"/exam"}
+              isLoading={this.state.isLoading}
+              setIsLoading={this.setLoading}
+            />
+            <PrivateRoute
+              component={PostSubmit}
+              isSignedIn={isSignedIn}
+              path={"/thankyou"}
+              isLoading={this.state.isLoading}
+              setIsLoading={this.setLoading}
+            />
+          </Router>
+        )}
+      </Layout>
+    );
+  }
 }
 
 export default App;
