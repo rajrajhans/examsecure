8f5ec69e1e03a3559a51e9ca562255bd9f3d6800
Raj Rajhans <raj.rajhans18@vit.edu>
2020-12-10 22:50:17 +0530

    Remove unnecessary styles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "tailwindcss/dist/base.css";
 import { css } from "styled-components/macro";
 import "./styles/bootstrapthemes.scss";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
