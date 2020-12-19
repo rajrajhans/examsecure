6eb5dff57bc47b2e2501df2d4c671bc0fb67c2a0
Raj Rajhans <raj.rajhans18@vit.edu>
2020-12-19 16:01:00 +0530

    Set up Redux and Redux DevTools, wrap App Component with Redux Provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./components/reducers";
 import "tailwindcss/dist/base.css";
 import { css } from "styled-components/macro";
 import "./styles/bootstrapthemes.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); //todo: after setting up env vars, call redux devtools only in dev environment and not in prod
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
