a52f7aafd667a8af4f5e062bf558ceda0eed4ca7
Raj Rajhans <raj.rajhans18@vit.edu>
2020-12-20 09:51:07 +0530

    Add questionsReducer

--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import questionSetsReducer from "./questionSetsReducer";
 
 export default combineReducers({
-  //reducer
+  questionsSetsState: questionSetsReducer,
 });
---
+++ src/components/reducers/questionsReducer.js
@@ -0,0 +1,5 @@
+const questionsReducer = (questionsState = [], action) => {
+  return questionsState;
+};
+
+export default questionsReducer;
