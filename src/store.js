20855e90f9a0b85a72154db1bf41e53ffa1e87d9
Raj Rajhans <raj.rajhans18@vit.edu>
2020-12-20 16:27:38 +0530

    Fix error when localhost used in browser without redux dev tools

--- src/store.js
+++ src/store.js
@@ -11,7 +11,9 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
   )
 ); //todo: after setting up env vars, call redux devtools only in dev environment and not in prod
 
