b6ec1c1d14626a19fb4da9ea2c7bf1ce4fd54036
Raj Rajhans <raj.rajhans18@vit.edu>
2020-12-20 14:00:24 +0530

    Add handleAnswers Lambda function

---
+++ lambdaFunctions/handleAnswers.js
@@ -0,0 +1,30 @@
+global.fetch = require("node-fetch");
+
+const { firebaseApiKey } = process.env;
+
+const respond = (httpStatusCode, response) => ({
+  isBase64Encoded: false,
+  statusCode: httpStatusCode,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(response),
+});
+
+exports.updateAnswer = async (event) => {
+  const body = JSON.parse(event.body);
+  let username = body.username;
+  let questionSetID = body.qSetID;
+  let questionID = body.qID;
+  let userSelectedAnswer = body.selectedAnswer;
+
+  let firebaseURL = `https://project2-e6924-default-rtdb.firebaseio.com/userAnswers/${username}/${questionSetID}/${questionID}.json?auth=${firebaseApiKey}`;
+
+  await fetch(firebaseURL, {
+    method: "put",
+    body: userSelectedAnswer,
+  }).catch(() => console.log("error"));
+
+  return respond(200, { status: "done " });
+};
