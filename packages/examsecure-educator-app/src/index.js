4221c1bd17c5363421f1c915f7f6b53aa4d1e14e
RajRajhans <me@rajrajhans.com>
2021-03-22 20:52:28 +0530

    Enable yarn workspaces and update dependencies to ensure consistency

--- packages/examsecure-educator-app/src/index.js
+++ packages/examsecure-educator-app/src/index.js
@@ -1,53 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import rootReducer from './reducers/rootReducer'
-import {createStore, applyMiddleware, compose} from 'redux'
-import {Provider, useSelector} from 'react-redux'
-import thunk from 'redux-thunk'
-import {createFirestoreInstance, reduxFirestore, getFirestore} from 'redux-firestore'
-import {ReactReduxFirebaseProvider, getFirebase, isLoaded} from 'react-redux-firebase'
-import fbConfig from './config/fb_config'
-import firebase from 'firebase/app'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import rootReducer from "./reducers/rootReducer";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider, useSelector } from "react-redux";
+import thunk from "redux-thunk";
+import {
+  createFirestoreInstance,
+  reduxFirestore,
+  getFirestore,
+} from "redux-firestore";
+import {
+  ReactReduxFirebaseProvider,
+  getFirebase,
+  isLoaded,
+} from "react-redux-firebase";
+import fbConfig from "./config/fb_config";
+import firebase from "firebase/app";
 
-const store = createStore(rootReducer, 
+const store = createStore(
+  rootReducer,
   compose(
-    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reduxFirestore(fbConfig),
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(fbConfig)
     //reactReduxFirebase(fbConfig, rrConfig)
-    
   )
 );
 
 const rrConfig = {
   useFirestoreForProfile: true,
-  userProfile: 'users',
-}
+  userProfile: "users",
+};
 
 const rrfProps = {
   firebase,
   config: rrConfig,
   dispatch: store.dispatch,
   createFirestoreInstance,
-  presence: 'presence',
-  sessions: 'sessions',
-}
+  presence: "presence",
+  sessions: "sessions",
+};
 
 function AuthIsLoaded({ children }) {
-  const auth = useSelector(state => state.firebase.auth)
+  const auth = useSelector((state) => state.firebase.auth);
   if (!isLoaded(auth)) return <div>Loading Screen...</div>;
-      return children
+  return children;
 }
 
 ReactDOM.render(
   <Provider store={store}>
     <ReactReduxFirebaseProvider {...rrfProps}>
-        <AuthIsLoaded>
-          <App />
-        </AuthIsLoaded>
-      </ReactReduxFirebaseProvider>
+      <AuthIsLoaded>
+        <App />
+      </AuthIsLoaded>
+    </ReactReduxFirebaseProvider>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
