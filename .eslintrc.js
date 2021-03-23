4f56866740de3c87a47c74b6240ce8b26c9c0863
RajRajhans <me@rajrajhans.com>
2021-03-23 14:41:29 +0530

    Add configurations for eslint, prettier, and lerna

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    plugins: ['jest'],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    extends: [
+        'airbnb/hooks',
+        'react-app',
+        'react-app/jest',
+        'plugin:jest/recommended',
+        'prettier',
+        'prettier/@typescript-eslint',
+        'prettier/react',
+    ],
+    env: {
+        node: true,
+        browser: true,
+        jest: true,
+        serviceworker: true,
+    },
+    ignorePatterns: ['build', '.*.js', '*.config.js', 'node_modules'],
+    rules: {
+        'import/no-extraneous-dependencies': ['error'],
+        'import/no-self-import': 'error',
+        'import/no-useless-path-segments': ['error', { noUselessIndex: true }],
+        'import/prefer-default-export': 'off',
+        'react/destructuring-assignment': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'react/prop-types': 'off',
+    },
+};
