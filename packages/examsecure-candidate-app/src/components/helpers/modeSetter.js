cda3ccf894365946d48ad44044840f1974624fce
RajRajhans <me@rajrajhans.com>
2021-03-30 00:49:22 +0530

    (dev): add api calls control based on dev/prod env

--- packages/examsecure-candidate-app/src/components/helpers/modeSetter.js
+++ packages/examsecure-candidate-app/src/components/helpers/modeSetter.js
@@ -1,4 +1,11 @@
 // Temporary hack to easily disable calls to AWS Lambda and Rekognition for testing purposes without incurring charges
-// TODO: Clean up and change it to use environment vars
+let mode = 0;
 
-export const mode = 1;
+if (
+  process.env.NODE_ENV === 'production' ||
+  process.env.REACT_APP_TURN_ON_API_CALLS === '1'
+) {
+  mode = 1;
+}
+
+export { mode };
