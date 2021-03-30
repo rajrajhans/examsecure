eb43dd34b980a356f352417cabb804031c988fa3
RajRajhans <me@rajrajhans.com>
2021-03-31 00:18:02 +0530

    (educator-app): add sign in screen

--- packages/examsecure-educator-app/src/App.js
+++ packages/examsecure-educator-app/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import routes from './routes';
+import SignIn from './authentication/SignIn';
 
 function App() {
-  return (
-    <div style={{ backgroundColor: 'white' }}>
-      <Router>
-        <Switch>
-          {routes.map((route, index) => (
-            <Route key={index} path={route.path} exact render={(props) => <route.component {...props} />} />
-          ))}
-        </Switch>
-      </Router>
-    </div>
-  );
+  // return (
+  //   <div style={{ backgroundColor: 'white' }}>
+  //     <Router>
+  //       <Switch>
+  //         {routes.map((route, index) => (
+  //           <Route key={index} path={route.path} exact render={(props) => <route.component {...props} />} />
+  //         ))}
+  //       </Switch>
+  //     </Router>
+  //   </div>
+  // );
+  return <SignIn />;
 }
 
 export default App;
