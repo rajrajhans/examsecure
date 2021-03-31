d7d95e1c2c0c7436f8ffaadb4da09fc1d7170f60
charmi2701 <ckdm27@gmail.com>
2021-03-31 13:41:59 +0530

    (eductor-app): Link sign-up page to firebase

--- packages/examsecure-educator-app/src/actions/auth_actions.js
+++ packages/examsecure-educator-app/src/actions/auth_actions.js
@@ -8,8 +8,27 @@ export const signIn = (credentials) => {
         ).then( () => {
             dispatch({type: 'LOGIN_SUCCESS'});
         }).catch((error) => {
-            dispatch({type: 'LOGIN_ERROR'});
+            dispatch({type: 'LOGIN_ERROR', error});
         });
+    }
+}
+
+export const signUp = (newUser) => {
+    return (dispatch, getState, {getFirebase}) => {
+        const firebase = getFirebase();
+        const database = firebase.database();
+        firebase.auth().createUserWithEmailAndPassword(
+            newUser.email,
+            newUser.pwd
+        ).then((resp) => {
+            return database.ref('educators/'+resp.user.uid).set({
+                name: newUser.name,
+            })
+        }).then(() => {
+            dispatch({type: 'SIGN_UP_SUCCESS'});
+        }).catch((error) => {
+            dispatch({type: 'SIGN_UP_FAILED'});
+        })
     }
 }
 
