bdbd496b6b821a31d3b9dd0f6e622d0672c6a573
RajRajhans <me@rajrajhans.com>
2021-09-05 21:47:43 +0530

    add useForm custom hook

---
+++ packages/examsecure-educator-app/src/utils/useForm.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+export default function useForm(initialState) {
+  const [inputState, setInputState] = useState();
+  const initialValues = Object.values(initialState).join('');
+
+  useEffect(() => {
+    setInputState(initialState);
+    // eslint-disable-next-line
+  }, [initialValues]);
+
+  const handleChange = (e) => {
+    let { value, name, type } = e.target;
+
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+
+    if (type === 'file') {
+      [value] = e.target.files;
+    }
+
+    setInputState((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const resetForm = () => {
+    setInputState(initialState);
+  };
+
+  return [inputState, handleChange, resetForm];
+}
