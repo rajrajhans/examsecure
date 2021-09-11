4d0760fdf4fa127347376d1b8120a8bd1a8c324d
RajRajhans <me@rajrajhans.com>
2021-09-11 15:50:12 +0530

    add Tests View with dummy data

--- packages/examsecure-educator-app/src/components/dashboard/Dashboard.js
+++ packages/examsecure-educator-app/src/components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Title } from '@examsecure/design-system';
 import './Dashboard.scss';
 import { Link } from 'react-router-dom';
+import SingleTestView from './SingleTestView';
 
 const Dashboard = () => {
   return (
@@ -11,6 +12,12 @@ const Dashboard = () => {
         <Link to={'/create-new-test'}>
           <Button label={'Create New Test'} variant={'secondary'} />
         </Link>
+      </div>
+
+      <div className="dash-tests-container">
+        {[1, 2, 3, 4].map((test) => (
+          <SingleTestView />
+        ))}
       </div>
     </div>
   );
