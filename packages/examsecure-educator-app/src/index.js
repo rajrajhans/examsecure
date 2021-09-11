05ae6da99d5317bea52d60356a4424d8fe75179d
RajRajhans <me@rajrajhans.com>
2021-09-11 12:03:06 +0530

    add react datetime css

--- packages/examsecure-educator-app/src/index.js
+++ packages/examsecure-educator-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-datetime/css/react-datetime.css';
 import './styles/index.scss';
 import rootReducer from './reducers/rootReducer';
 import { createStore, applyMiddleware, compose } from 'redux';
