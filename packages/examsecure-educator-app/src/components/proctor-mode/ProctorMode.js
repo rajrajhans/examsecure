440f0c4c00d1ddc0ccf20a2b8615683d1102413f
RajRajhans <me@rajrajhans.com>
2021-09-11 19:50:13 +0530

    update URL when use changes left panel item

--- packages/examsecure-educator-app/src/components/proctor-mode/ProctorMode.js
+++ packages/examsecure-educator-app/src/components/proctor-mode/ProctorMode.js
@@ -2,11 +2,17 @@ import React, { useState } from 'react';
 import TopBar from './components/TopBar';
 import LeftPanel from './components/LeftPanel';
 import ProctorModeContentWrapper from './components/ProctorModeContentWrapper';
+import { useHistory } from 'react-router-dom';
 
 const ProctorMode = ({ testID, view }) => {
   const [currentView, setCurrentView] = useState(view);
+  const history = useHistory();
 
   const changeCurrentView = (newView) => {
+    history.push({
+      pathname: '/proctor-mode',
+      search: `?test=${testID}&view=${newView}`,
+    });
     setCurrentView(newView);
   };
 
