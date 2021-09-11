0d300d898dd58b2ca1ca7a65477922ef131f19c1
RajRajhans <me@rajrajhans.com>
2021-09-12 00:49:55 +0530

    update proctor mode component to include title and descriptions

--- packages/examsecure-educator-app/src/components/proctor-mode/proctor-mode-views/TestOverview.js
+++ packages/examsecure-educator-app/src/components/proctor-mode/proctor-mode-views/TestOverview.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Title } from '@examsecure/design-system';
+import OverviewPane from '../../create-test/overview-pane/OverviewPane';
 
 const TestOverview = () => {
   return (
     <div>
       <Title value={'Test Overview'} />
+      <OverviewPane />
     </div>
   );
 };
--- packages/examsecure-educator-app/src/components/proctor-mode/proctor-mode-views/TestQuestions.js
+++ packages/examsecure-educator-app/src/components/proctor-mode/proctor-mode-views/TestQuestions.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Title } from '@examsecure/design-system';
+import QuestionsPane from '../../create-test/questions-pane/QuestionsPane';
 
 const TestQuestions = () => {
   return (
     <div>
       <Title value={'Test Questions'} />
+      <QuestionsPane />
     </div>
   );
 };
