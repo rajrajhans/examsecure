e3cae6cb364d528461b56bf9f0ecec335b71688d
RajRajhans <me@rajrajhans.com>
2021-09-14 01:09:18 +0530

    add create_tests action

---
+++ packages/examsecure-educator-app/src/actions/create_tests.js
@@ -0,0 +1,21 @@
+import { startLoading, stopLoading } from './loading_actions';
+
+export const create_test_action = (testDetails) => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    const database = firebase.database();
+
+    dispatch(startLoading());
+
+    database
+      .ref('tests/')
+      .push(testDetails)
+      .then(() => {
+        dispatch(stopLoading());
+      })
+      .catch((error) => {
+        dispatch(stopLoading());
+        console.log('ERROR while creating new test:', error);
+      });
+  };
+};
