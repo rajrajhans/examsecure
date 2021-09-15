5855d636cc0db2ab60571f38d292c68abf7b21dc
RajRajhans <me@rajrajhans.com>
2021-09-15 21:49:42 +0530

    add MCQMultipleChoicesInput.js

---
+++ packages/examsecure-educator-app/src/components/create-test/questions-pane/add-question-modal/MCQMultipleChoicesInput.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+const MCQMultipleChoicesInput = ({
+  choiceSelectChangeHandler,
+  choiceTextChangeHandler,
+  inputs,
+}) => {
+  return (
+    <Form.Group className="qp-add-new-question-choices-container">
+      <Form.Label>Answer Choices</Form.Label>
+      {inputs.choices.map(({ id, choice_text }) => (
+        <Form.Check key={id}>
+          <Form.Check.Input
+            type={'checkbox'}
+            name={'mcq_multiple_choice_option'}
+            id={id}
+            onChange={choiceSelectChangeHandler}
+          />
+          <Form.Check.Label>
+            <Form.Group>
+              <Form.Control
+                type="text"
+                placeholder={`Option ${id} `}
+                id={id}
+                onChange={choiceTextChangeHandler}
+                value={choice_text}
+              />
+            </Form.Group>
+          </Form.Check.Label>
+        </Form.Check>
+      ))}
+
+      <Form.Text muted>
+        Please choose the correct answers among the options
+      </Form.Text>
+    </Form.Group>
+  );
+};
+
+export default MCQMultipleChoicesInput;
