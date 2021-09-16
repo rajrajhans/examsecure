828969d967ec65ba93626884396d8bd1fea3abe8
RajRajhans <me@rajrajhans.com>
2021-09-16 12:05:38 +0530

    fetch test and route user to selected tests in ProctorModeTestSelector.js

--- packages/examsecure-educator-app/src/components/proctor-mode/ProctorModeTestSelector.js
+++ packages/examsecure-educator-app/src/components/proctor-mode/ProctorModeTestSelector.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Title } from '@examsecure/design-system';
 import { Form } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useFirebaseConnect } from 'react-redux-firebase';
+import LoadingSpinner from '../helpers/LoadingSpinner';
+import SingleTestView from '../dashboard/SingleTestView';
 
 const ProctorModeTestSelector = () => {
   const router = useHistory();
+  const [selectedTestID, setSelectedTestID] = useState('');
+
+  const uid = useSelector((state) => state.firebase.auth.uid);
+  useFirebaseConnect(() => [{ path: `tests/${uid}` }]);
+  const testsWrapper = useSelector((state) => state.firebase.data.tests);
+  const tests = testsWrapper && testsWrapper[uid];
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSelectedTestID(value);
+  };
 
   const handleGoToDashboard = () => {
-    router.push('/proctor-mode/proctor_dashboard?test=123123');
+    router.push(`/proctor-mode/proctor_dashboard?test=${selectedTestID}`);
   };
 
   return (
@@ -23,15 +38,24 @@ const ProctorModeTestSelector = () => {
             Please select a test to proceed
           </div>
           <div className="proc-test-selector">
-            <Form.Group>
-              <Form.Control as={'select'}>
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((id) => (
-                  <option value={'test1'} key={id}>
-                    Test {id}
-                  </option>
-                ))}
-              </Form.Control>
-            </Form.Group>
+            {!tests ? (
+              <div className="loading-spinner">
+                <LoadingSpinner />
+              </div>
+            ) : (
+              <div className="dash-tests-container">
+                <Form.Group>
+                  <Form.Control as={'select'} onChange={handleChange}>
+                    <option value="">Select a Test</option>
+                    {Object.entries(tests)?.map(([id, test]) => (
+                      <option value={id} key={id}>
+                        {test.test_name}
+                      </option>
+                    ))}
+                  </Form.Control>
+                </Form.Group>
+              </div>
+            )}
           </div>
           <Button
             label={'Go to Proctor Dashboard'}
